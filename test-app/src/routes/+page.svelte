1300c03fb1118648a060830c84c6e37cb2e9f395
b-straub <b-straub@users.noreply.github.com>
2024-07-21 18:22:23 +0000

    Initial app

---
+++ test-app/src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import Button, { Label } from '@smui/button';
+    
+	let count = 1;
+
+	// the `$:` means 're-run whenever these values change'
+	$: doubled = count * 2;
+	$: quadrupled = doubled * 2;
+
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<Button on:click={handleClick} variant="raised">
+    <Label>
+        Count: {count}
+    </Label>
+</Button>
+
+<p>{count} * 2 = {doubled}</p>
+<p>{doubled} * 2 = {quadrupled}</p>
+
